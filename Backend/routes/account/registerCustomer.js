723659fc4f02e9e7c1b7da0f9c70cb1b86ba912f
kenamehta <kenamehta1995@gmail.com>
2020-04-13 16:20:21 -0700

    Routes added for login and register

--- Backend/routes/account/registerCustomer.js
+++ Backend/routes/account/registerCustomer.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const kafka = require("../../kafka/client");
 
 //Register a customer. Adds new student record in model customerRegister
-router.post("/registerCustomer", async function(req, res) {
+router.post("/", async function(req, res) {
   let msg = req.body;
   msg.route = "registerCustomer";
   kafka.make_request("account", msg, function(err, results) {
