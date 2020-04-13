a0416330cba581e988ecabbfe9b6bc6f5e8d589a
puneetjyot <puneetjyot@gmail.com>
2020-04-13 16:25:06 -0700

    Frontend customer profile done

--- Frontend/src/app/container/customer/profile/CustomerProfile.js
+++ Frontend/src/app/container/customer/profile/CustomerProfile.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import ProfilePic from './ProfilePic'
-import  Insights from "./Insights";
+import React, { Component } from "react";
+import ProfilePic from "./ProfilePic";
+import Insights from "./Insights";
 
 class CustomerProfile extends Component {
-    state = {  }
-    render() { 
-        return ( <div>
+  state = {};
+  render() {
+    return (
+      <div>
         <ProfilePic></ProfilePic>
         <Insights></Insights>
-        </div> );
-    }
+      </div>
+    );
+  }
 }
- 
+
 export default CustomerProfile;
