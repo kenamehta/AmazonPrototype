f9349c04f5587849a1b1195b44a80df56403d768
kenamehta <kenamehta1995@gmail.com>
2020-04-14 16:38:06 -0700

    updated model

--- KafkaBackend/models/seller.model.js
+++ KafkaBackend/models/seller.model.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
 
 const sellerProfile = new Schema(
   {
-    emailId: { type: String, required: true },
+    emailId: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     phone: { type: String },
     profilePictureUrl: { type: String, default: "default.png" },
@@ -17,5 +18,6 @@ const sellerProfile = new Schema(
     timestamps: true
   }
 );
+sellerProfile.plugin(uniqueValidator);
 
 module.exports = mongoose.model("seller", sellerProfile);
