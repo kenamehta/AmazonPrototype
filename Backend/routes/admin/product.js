d4eed26f032163eb72cf3f21c4d2836003475ea5
sarthakjain27 <sarthakssj@gmail.com>
2020-04-20 11:08:34 -0700

    delete Product Category admin backend api

--- Backend/routes/admin/product.js
+++ Backend/routes/admin/product.js
@@ -4,11 +4,25 @@ const router = express.Router();
 const { checkAuth } = require("../../passport");
 const kafka = require("../../kafka/client");
 
-router.post("/addProductCategory", checkAuth, (req, res) => {
+router.post("/addProductCategory", (req, res) => {
   console.log("Inside post of product/admin/addProductCategory");
   console.log(req.body);
 
   req.body.path = "addProductCategory";
+  kafka.make_request("adminProductService", req.body, function (err, results) {
+    if (err) {
+      res.status(500).send("System Error");
+    } else {
+      res.status(results.status).send(results.message);
+    }
+  });
+});
+
+router.post('/deleteProductCategory', (req, res) => {
+  console.log("Inside post of product/admin/deleteProductCategory");
+  console.log(req.body);
+
+  req.body.path = "deleteProductCategory";
   kafka.make_request("adminProductService", req.body, function (err, results) {
     if (err) {
       res.status(500).send("System Error");
