d395cad271612c3f3712a18b7747d52fc1c6bea5
kenamehta <kenamehta1995@gmail.com>
2020-04-24 14:56:06 -0700

    pull

--- Frontend/src/app/container/customer/cartAndSaved/SavedProducts.js
+++ Frontend/src/app/container/customer/cartAndSaved/SavedProducts.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Image,Col } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 class SavedProducts extends Component {
@@ -7,93 +7,91 @@ class SavedProducts extends Component {
   render() {
     return (
       <Container>
-        <Col md={9}>
-          <div className="mt-3">
-            <h5>Saved for later ({this.props.savedCnt} Items)</h5>
-            <div className="style__divider___1j_Fp mt-2" />
+        <div className="mt-3">
+          <h5>Saved for later ({this.props.savedCnt} Items)</h5>
+          <div className="style__divider___1j_Fp mt-2" />
 
-            {this.props.savedProductsArr ? (
-              this.props.savedProductsArr.map(product => (
-                <div>
-                  <div className="d-flex">
-                    <div
-                      className="col-2"
+          {this.props.savedProductsArr ? (
+            this.props.savedProductsArr.map(product => (
+              <div>
+                <div className="d-flex">
+                  <div
+                    className="col-2"
+                    style={{
+                      width: "12.499999995%",
+                      flex: "0 0 12.499%",
+                      maxWidth: "12.499%"
+                    }}
+                  >
+                    <Image
+                      src={product ? product.photos[0] : ""}
+                      className="productImg"
                       style={{
-                        width: "12.499999995%",
-                        flex: "0 0 12.499%",
-                        maxWidth: "12.499%"
-                      }}
-                    >
-                      <Image
-                        src={product ? product.photos[0] : ""}
-                        className="productImg"
-                        style={{
-                          //width: "130px",
-                          height: "130px",
-                          paddingTop: "10px",
-                          paddingBottom: "10px",
-                          margin: "auto 0"
-                        }}
-                      />
-                    </div>
-                    <div
-                      className="col-10"
-                      style={{
+                        //width: "130px",
+                        height: "130px",
                         paddingTop: "10px",
                         paddingBottom: "10px",
-                        fontSize: "17px"
+                        margin: "auto 0"
                       }}
-                    >
-                      <div className="justify-content-between d-flex">
-                        <div>
-                          <Link
-                            className="name_style"
-                            to={`/productPage/${product._id}`}
-                          >
-                            {product.productName}
-                          </Link>
-                        </div>
-                        <div className="mr-5 price_style">
-                          ${product.productPrice}
-                        </div>
+                    />
+                  </div>
+                  <div
+                    className="col-10"
+                    style={{
+                      paddingTop: "10px",
+                      paddingBottom: "10px",
+                      fontSize: "17px"
+                    }}
+                  >
+                    <div className="justify-content-between d-flex">
+                      <div>
+                        <Link
+                          className="name_style"
+                          to={`/productPage/${product._id}`}
+                        >
+                          {product.productName}
+                        </Link>
                       </div>
-                      <div class="stock_style">In stock</div>
-                      <div className="pt-2 action_style">
-                        <span
-                          className="mr-2 delete_style"
-                          onClick={() => {
-                            this.props.deleteSavedProduct({
-                              data: {
-                                data: { productId: product._id }
-                              }
-                            });
-                          }}
-                        >
-                          Delete
-                        </span>
-                        <span className="mr-2">|</span>
-                        <span
-                          className="move_style"
-                          onClick={() => {
-                            this.props.moveSavedToCart({
-                              productId: product._id,
-                              individualProductPrice: product.productPrice
-                            });
-                          }}
-                        >
-                          Move to Cart
-                        </span>
+                      <div className="mr-5 price_style">
+                        ${product.productPrice}
                       </div>
                     </div>
+                    <div class="stock_style">In stock</div>
+                    <div className="pt-2 action_style">
+                      <span
+                        className="mr-2 delete_style"
+                        onClick={() => {
+                          this.props.deleteSavedProduct({
+                            data: {
+                              data: { productId: product._id }
+                            }
+                          });
+                        }}
+                      >
+                        Delete
+                      </span>
+                      <span className="mr-2">|</span>
+                      <span
+                        className="move_style"
+                        onClick={() => {
+                          this.props.moveSavedToCart({
+                            productId: product._id,
+                            individualProductPrice: product.productPrice
+                          });
+                        }}
+                      >
+                        Move to Cart
+                      </span>
+                    </div>
                   </div>
-                  <div className="style__divider___1j_Fp mt-2" />
                 </div>
-              ))
-            ) : (
-              ""
-            )}
-          </div>
-        </Col>
+                <div className="style__divider___1j_Fp mt-2" />
+              </div>
+            ))
+          ) : (
+            ""
+          )}
+        </div>
       </Container>
     );
   }
