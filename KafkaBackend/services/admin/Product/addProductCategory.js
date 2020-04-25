af4a48da7e1c43724d4a466a8caef92d57926676
sarthakjain27 <sarthakssj@gmail.com>
2020-04-25 11:48:10 -0700

    Update KafkaBackend API for addProductCategory and deleteProductCategory

--- KafkaBackend/services/admin/Product/addProductCategory.js
+++ KafkaBackend/services/admin/Product/addProductCategory.js
@@ -4,20 +4,49 @@ const category = require("../../../models/category.model");
 const addProductCategory = (msg, callback) => {
   var res = {};
 
-  let categoryToCreate = category({
-    name: msg.categoryName
-  });
-
-  categoryToCreate.save((categorySaveError) => {
-    if(categorySaveError){
+  category.findOne({ name:{'$regex':'^'+msg.categoryName+'$',$options:'i'}},(err, existingCategory) => {
+    if(err){
       res.status = 500;
       res.message = 'Database Error';
-    } else {
-      res.status = 200;
-      res.message = 'Success';
+      callback(null, res);
     }
-    callback(null, res);
+    if(!existingCategory){
+      let categoryToCreate = category({
+        name: msg.categoryName
+      });
+      categoryToCreate.save((categorySaveError) => {
+        if(categorySaveError){
+          res.status = 500;
+          res.message = 'Database Error';
+          callback(null, res);
+        } else {
+          category.find({},(err, result) => {
+            if(err){
+              res.status = 500;
+              res.message = 'Database Error';
+              callback(null, res);
+            } 
+            res.status = 200;
+            res.message = result;
+            callback(null, res);
+          });
+        }
+      })
+    } else {
+      category.find({},(err, result) => {
+        if(err){
+          res.status = 500;
+          res.message = 'Database Error';
+          callback(null, res);
+        } 
+        res.status = 200;
+        res.message = result;
+        callback(null, res);
+      });
+    }
   })
+
+  
 }
 
 exports.addProductCategory = addProductCategory;
