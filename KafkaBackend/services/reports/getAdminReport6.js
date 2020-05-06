0f7f61e7d6871edf65e60a7abc1a2c7a79466f52
kenamehta <kenamehta1995@gmail.com>
2020-05-06 15:24:11 -0700

    admin report 6

--- KafkaBackend/services/reports/getAdminReport6.js
+++ KafkaBackend/services/reports/getAdminReport6.js
@@ -11,51 +11,16 @@ let getAdminReport6 = async (msg, callback) => {
     var dd = String(today.getDate()).padStart(2, "0");
     var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
     var yyyy = today.getFullYear();
-
     today = mm + "/" + dd + "/" + yyyy;
     console.log(today.toString());
-
-    const clicks = await clickCount.find({});
-    console.log(clicks);
-
-    return callback(null, { status: 200 });
-    // const products = await product.find();
-    // if (products) {
-    //   let clickArr = [];
-    //   products.map(item => {
-    //     let lastClick = item.clickCount ? item.clickCount.slice(-1).pop() : {};
-    //     console.log("lastClick");
-    //     console.log(lastClick);
-    //     clickArr.push({
-    //       productName: item.productName,
-    //       productId: item._id,
-    //       clickCount: lastClick.count
-    //     });
-    //   });
-    //   //   console.log("clickArr");
-    //   //   console.log(clickArr);
-    //   function compare(a, b) {
-    //     const countA = a.count;
-    //     const countB = b.count;
-    //     let comparison = 0;
-    //     if (countA < countB) {
-    //       comparison = 1;
-    //     } else if (countA > countB) {
-    //       comparison = -1;
-    //     }
-    //     return comparison;
-    //   }
-    //   //   clickArr.sort(compare);
-    //   //   console.log("clickArr");
-    //   //   console.log(clickArr);
-    //   let first10 = clickArr.slice(1, 11);
-    //   return callback(null, {
-    //     status: 200,
-    //     adminReport6: first10
-    //   });
-    // } else {
-    //   return callback({ status: 403 }, null);
-    // }
+    const clicks = await clickCount
+      .find({
+        lastViewedCustomDate: today.toString()
+      })
+      .select({ lastViewedCustomDate: 1, count: 1, productName: 1, _id: 0 })
+      .sort({ count: -1 })
+      .limit(10);
+    return callback(null, { status: 200, clicksArr: clicks });
   } catch (err) {
     console.log(err);
   }
