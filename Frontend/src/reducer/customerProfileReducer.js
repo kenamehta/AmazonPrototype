cd108ecfb65b9c5d21c81b530befd69fcbe0520e
kenamehta <kenamehta1995@gmail.com>
2020-05-07 12:24:28 -0700

    logout added in customer profile reducer

--- Frontend/src/reducer/customerProfileReducer.js
+++ Frontend/src/reducer/customerProfileReducer.js
@@ -9,18 +9,19 @@ import {
   ADDORUPDATEPAYMENT,
   DELETEPAYMENT
 } from "../action/customerprofileaction/actionType";
+import { LOGOUT } from "../action/UserAction/actionTypes";
 
 const initialState = {
   profiledata: {},
-  paymentSelectModal: "none",
+  paymentSelectModal: "none"
 };
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case GETPROFILE:
       return {
         ...state,
-        profiledata: action.payload,
+        profiledata: action.payload
       };
     case UPDATEPROFILE:
       console.log(state);
@@ -34,10 +35,10 @@ export default function (state = initialState, action) {
               ...state.profiledata.data.mainCustomer,
               name: action.payload.data.name,
               city: action.payload.data.city,
-              state: action.payload.data.state,
-            },
-          },
-        },
+              state: action.payload.data.state
+            }
+          }
+        }
       };
     case UPDATEPROFILEPIC:
       return {
@@ -48,49 +49,62 @@ export default function (state = initialState, action) {
             ...state.profiledata.data,
             mainCustomer: {
               ...state.profiledata.data.mainCustomer,
-              profilePictureUrl: action.payload.profilePictureUrl,
-            },
-          },
-        },
+              profilePictureUrl: action.payload.profilePictureUrl
+            }
+          }
+        }
       };
     case GETADDRESS:
       return {
         ...state,
-        addressArray: action.payload,
+        addressArray: action.payload
       };
     case ADDADDRESS:
       return {
         ...state,
         addressArray: action.payload,
-        msgSuccess: "Address added/edited successfully",
+        msgSuccess: "Address added/edited successfully"
       };
 
     case DELETEADDRESS:
       return {
         ...state,
-        addressArray: action.payload,
+        addressArray: action.payload
       };
     case GETPAYMENT:
       return {
         ...state,
-        paymentArr: action.payload,
+        paymentArr: action.payload
       };
     case ADDORUPDATEPAYMENT:
       return {
         ...state,
         paymentArr: action.payload,
-        msgSuccess: "Card added/edited successfully",
+        msgSuccess: "Card added/edited successfully"
       };
     case DELETEPAYMENT:
       return {
         ...state,
-        paymentArr: action.payload,
+        paymentArr: action.payload
       };
 
     case "SENDMODALSTATUS":
       return {
         ...state,
-        ...action.payload,
+        ...action.payload
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        profiledata: {
+          ...state.profiledata,
+          data: {
+            ...state.profiledata.data,
+            mainCustomer: {},
+            insights: {},
+            comment_cnt: 0
+          }
+        }
       };
     default:
       return state;
